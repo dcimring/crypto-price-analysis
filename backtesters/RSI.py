1c42353395feb96923c8dceca4bbb4c6e9f402ee
Daniel Cimring <daniel@blackhatmedia.com>
2018-06-26 11:08:03 -0500

    Implement RSI strategy
    There are may different ones. This is based on momentum so go long when RSI is high, short when it is low, and out of the market when in between

--- backtesters/RSI.py
+++ backtesters/RSI.py
@@ -17,16 +17,23 @@ class RSIBacktester(Backtester):
     long_only: (boolean) True if the strategy can only go long
     '''
 
-    def __init__(self, series, lookback=14, buy_on=30, sell_on=70, long_only=False):
+    def __init__(self, series, lookback=14, buy_on=70, sell_on=30, long_only=False):
         self._lookback=lookback
         self._buy_on=buy_on
         self._sell_on=sell_on
-        super().__init__(self,series,long_only=long_only)
+        Backtester.__init__(self,series,long_only=long_only)
 
     def __str__(self):
         return "RSI Backtest Strategy (lookback=%d, buy_on=%d, sell_on=%d, long_only=%s, start=%s, end=%s)" % (
             self._lookback, self._buy_on, self._sell_on, str(self._long_only), str(self._start_date), str(self._end_date))
 
+    def plot(self, start_date=None, end_date=None, figsize=None):
+        Backtester.plot(self,start_date=start_date,end_date=end_date,figsize=figsize)
+        temp = self._df.loc[start_date:end_date]
+        plt.legend()
+        plt.show()
+        temp['RSI'].plot(figsize=(figsize[0],4))
+        plt.show()
 
     def _trade_logic(self):
         '''Implements the trade logic in order to come up with
@@ -44,7 +51,13 @@ class RSIBacktester(Backtester):
         RSI = 100.0 - (100.0 / (1.0 + RS))
         self._df['RSI'] = RSI
 
+        # long when RSI is high, short when it is low, in cash otherwise
+        # there are other strategies
+        # https://tradingsim.com/blog/rsi-relative-strength-index/
 
+        self._df['stance'] = np.where(self._df['RSI'] >= self._buy_on, 1, 0)
 
-        self._df['stance'] = 1
+        if not self._long_only:
+            self._df['stance'] = np.where(self._df['RSI'] <= self._sell_on, -1, self._df['stance'])
+
 
