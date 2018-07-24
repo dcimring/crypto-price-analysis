5bc17ce5dff288e534ceee80ffa67ecc8ca84af1
Daniel Cimring <daniel@blackhatmedia.com>
2018-07-24 15:47:06 -0500

    Improve strategy
    Some confusion over what high and low params are for TA_LIB function. I am assuming its the daily high and low, and I am using daily close when that is not avail

--- backtesters/sar.py
+++ backtesters/sar.py
@@ -17,21 +17,27 @@ class SARBacktester(Backtester):
 
     Parameters:
     series: (Panda Series) a list of CLOSE prices by date
-    lookback: (int) lookback period for calculation of highs and lows
     acceleration: (float) determines the sensitivity of the SAR
     maximum: (float) maximum for acceleration
     long_only: (boolean) True if the strategy can only go long
     '''
 
-    def __init__(self, series, lookback=30, acceleration=0.02, maximum=0.2, long_only=False):
+    def __init__(self, series, high=None, low=None, acceleration=0.02, maximum=0.2, long_only=False):
         self._acceleration = acceleration
         self._maximum = maximum
-        self._lookback = lookback
         super(SARBacktester,self).__init__(series,long_only=long_only)
+        if high is not None:
+            self._df['high'] = high
+        else:
+            self._df['high'] = series
+        if low is not None:
+            self._df['low'] = low
+        else:
+            self._df['low'] = series
 
     def __str__(self):
-        return "Parabolic SAR Backtest Strategy (lookback=%d, acceleration=%0.3f, maximum=%0.3f, long_only=%s, start=%s, end=%s)" % (
-            self._lookback, self._acceleration, self._maximum, str(self._long_only), str(self._start_date), str(self._end_date))
+        return "Parabolic SAR Backtest Strategy (acceleration=%0.3f, maximum=%0.3f, long_only=%s, start=%s, end=%s)" % (
+            self._acceleration, self._maximum, str(self._long_only), str(self._start_date), str(self._end_date))
 
     def plot(self, start_date=None, end_date=None, figsize=None):
         sns.set_style("white")
@@ -48,8 +54,9 @@ class SARBacktester(Backtester):
         a set of stances
         '''
 
-        self._df['high'] = self._df['last'].rolling(window=self._lookback).max()  # MAX(close, timeperiod=30)
-        self._df['low'] = self._df['last'].rolling(window=self._lookback).min()  # low = MIN(close, timeperiod=30)
+        #self._df['high'] = self._df['last'].rolling(window=self._lookback).max()  # MAX(close, timeperiod=30)
+        #self._df['low'] = self._df['last'].rolling(window=self._lookback).min()  # low = MIN(close, timeperiod=30)
+
         self._df['sar'] = talib.SAR(self._df['high'], self._df['low'], acceleration=self._acceleration, maximum=self._maximum)
 
         self._df['stance'] = np.where(self._df['last'] >= self._df['sar'], 1, 0)
