481d89439871691612d1fa9f01293079c087e9d2
Daniel Cimring <daniel@blackhatmedia.com>
2018-08-03 14:44:31 -0500

    Rewrite portfolio
    Allows for mixture of strategies on different assets, over different time spans, and weights for each strategy

--- backtesters/portfolio.py
+++ backtesters/portfolio.py
@@ -7,20 +7,44 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from backtester import Backtester
+from IPython.core.debugger import set_trace
+
 
 class PortfolioBacktester(Backtester):
     '''Backtest a combination of strategies
 
     Parameters:
     strategies: (list) a list of strategies
+    weights: (list) optional list of weights, if None then equal weights assumed
     '''
 
-    def __init__(self, strategies=None):
+    def __init__(self, strategies=None, weights=None):
+        
         if not strategies:
             raise ValueError('Strategy list is empty')
+        
+        if weights is None:
+            self._weights = np.ones(len(strategies)) / len(strategies)
+        else:
+            if len(weights) != len(strategies): raise ValueError('Stategy and Weighs mismatch')
+            if not np.isclose(sum(weights),1): weights = np.array(weights) / float(sum(weights))
+            self._weights = weights
+
         self._strategies = strategies
         self._has_run = False
         self._long_only = False;
+
+        start_dates, end_dates = [],[]
+        for s in self._strategies:
+            start_dates.append(s._df.index[0])
+            end_dates.append(s._df.index[-1])
+
+        self._start_date = min(start_dates)
+        self._end_date = max(end_dates)
+
+        index = pd.DatetimeIndex(start = self._start_date, end = self._end_date, freq='d')
+        self._df = pd.DataFrame(index=index, columns=['strategy','market','stance','trade']).fillna(0)
+
 
     def __str__(self):
         r = ''
@@ -34,14 +58,43 @@ class PortfolioBacktester(Backtester):
 
         for i, s in enumerate(self._strategies):
             s._run()
-            if i==0:
-                self._df = s._df['last'].copy().to_frame()
-                self._df.columns = ['last']
-                self._df['stance'] = s._df['stance']
-                #self._df.head(10)
-                continue
-            self._df['stance'] += s._df['stance']
+            self._df['stance'] +=  s._df.reindex(self._df.index, fill_value=0)['stance']
+            self._df['trade'] +=  s._df.reindex(self._df.index, fill_value=0)['trade']
+            self._df['market'] += s._df.reindex(self._df.index, fill_value=0)['market'] * self._weights[i]
+            self._df['strategy'] += s._df.reindex(self._df.index, fill_value=0)['strategy'] * self._weights[i]
 
-        self._df['stance'] = self._df['stance'] / len(self._strategies)
+
+    def _run(self):
+
+        if self._has_run:
+            return
+
+        self._trade_logic()
+
+        self._df['strategy_last'] = self._df['strategy'].cumsum().apply(np.exp).dropna()
+
+        # since we are using log returns multiplying by -1 for shorts works
+
+        start_date, end_date = self._df.index[0], self._df.index[-1]
+        years = (end_date - start_date).days / 365.25
+
+        #self._df['trade'] = np.where(self._df['stance'] != self._df['stance'].shift(1).fillna(0), 1, 0)
+        trades = self._df['trade'].sum()
+        market = ((np.exp(self._df['market'].cumsum()[-1]) - 1) * 100)
+        market_pa = ((market / 100 + 1) ** (1 / years) - 1) * 100
+        strategy = ((np.exp(self._df['strategy'].cumsum()[-1]) - 1) * 100)
+        strategy_pa = ((strategy / 100 + 1) ** (1 / years) - 1) * 100
+        #set_trace()
+        # Calculating sharpe using log returns
+        # For daily data you annualise with sqrt(365.25)
+        sharpe = math.sqrt(365.25) * np.average(self._df['strategy'].dropna()) / np.std(self._df['strategy'].dropna())
+        market_sharpe = math.sqrt(365.25) * np.average(self._df['market'].dropna()) / np.std(self._df['market'].dropna())
+        self._results = {"Strategy":np.round(strategy,2), "Market":np.round(market,2),"Trades":trades,"Sharpe":np.round(sharpe,2),
+                        "Strategy_pa": np.round(strategy_pa,2), "Market_pa": np.round(market_pa,2), "Years": np.round(years,2),
+                        "Trades_per_month":np.round(trades/years/12,2),"Market_sharpe":np.round(market_sharpe,2)}
+        self._has_run = True
+
+        
+
         
 
